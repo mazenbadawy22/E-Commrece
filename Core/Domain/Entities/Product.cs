0c2bddfefdc9e2f6b8138823518774507a1388c5
mazenbadawy22 <mazenbbadawy2003@gmail.com>
2025-03-26 08:28:40 +0200

    API01

---
+++ Core/Domain/Entities/Product.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Entities
+{
+    public class Product:BaseEntity<int>
+    {
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public string PictureUrl { get; set; }
+        public decimal Price { get; set; }
+        #region ProductBrand
+        public int BrandId { get; set; }
+        public ProductBrand productBrand { get; set; }
+        #endregion
+        #region ProductType
+        public int TypeId { get; set; }
+        public ProductType productType { get; set; }
+        #endregion
+    }
+}
